b4914f0ad7d9b06a6b74676edab2912f0ec9c4b4
dilane3 <komboudilane125@gmail.com>
2024-01-11 22:33:51 +0100

    Changing the welcome page design by removing the counter and changing styles for both blank javascript and typescript template

--- templates/javascript/src/pages/home.page.jsx
+++ templates/javascript/src/pages/home.page.jsx
@@ -1,120 +1,60 @@
-import React, { useEffect, useState } from "react";
-import { PageComponent, defineRoutePage } from "rasengan";
+import React from "react";
+import { PageComponent, defineRoutePage, Link } from "rasengan";
 
 class Home extends PageComponent {
   render() {
-    // Local state
-    const [chrono, setChrono] = useState(60);
-    const [isRunning, setIsRunning] = useState(false);
-
-    // Effects
-    useEffect(() => {
-      if (isRunning) {
-        const interval = setInterval(() => {
-          setChrono((chrono) => chrono - 1);
-        }, 1000);
-
-        return () => clearInterval(interval);
-      }
-    }, [isRunning]);
-
-    useEffect(() => {
-      if (chrono < 0) {
-        handleReset();
-      }
-    }, [chrono]);
-
-    // Handlers
-    const handleReset = () => {
-      setChrono(60);
-      setIsRunning(false);
-    };
-
-    const handleStart = () => {
-      setIsRunning(true);
-    };
-
-    const handleStop = () => {
-      setIsRunning(false);
-    };
-
     return (
-      <section className="app">
-        <div>
-          <h1>
-            Welcome to <span>Rasengan.js</span>
+      <section className="main">
+        <header className="header">
+          <div>
+            <span>Powered by</span>
+            <Link to="https://rasengan.dev" target="_blank">
+              <span className="logo-text">Rasengan</span>
+            </Link>
+          </div>
+        </header>
+
+        <div className="hero">
+          <h1 className="title">
+            Welcome to <span>Rasengan</span>
           </h1>
-        </div>
-
-        <section className="counter_container">
-          <h2>Chrono</h2>
-
-          <div className="counter_value">
-            <span>{chrono}</span>
-          </div>
-
-          <div className="counter_buttons">
-            <button onClick={handleReset}>Reset</button>
-
-            {isRunning ? (
-              <button onClick={handleStop}>Stop</button>
-            ) : (
-              <button onClick={handleStart}>Start</button>
-            )}
-          </div>
-        </section>
-
-        <div className="instruction">
-          <p>
-            Update the <code>src/pages/home.page.jsx</code> file and save to
-            reload.
+          <p className="subtitle">
+            To get started, edit the file <code>src/pages/home.page.jsx</code>
           </p>
         </div>
 
-        <hr />
-
-        <h2>Learn more</h2>
-
-        <section className="links">
-          <div className="block">
-            <div className="link">
-              <span>Documentation</span>
-
-              <span>
-                Lorem, ipsum dolor sit amet consectetur adipisicing elit. Nemo,
-                tempore.
-              </span>
-            </div>
-
-            <div className="link">
-              <span>Examples</span>
-
-              <span>
-                Lorem, ipsum dolor sit amet consectetur adipisicing elit. Nemo,
-                tempore.
-              </span>
-            </div>
+        <div className="instructions">
+          <div className="card">
+            <h2 className="title">Documentation</h2>
+            <p>Find in-depth information about Rasengan features and API.</p>
+            <Link to="https://rasengan.dev/docs" target="_blank">
+              Read the Docs
+            </Link>
           </div>
 
-          <div className="block">
-            <div className="link">
-              <span>Github</span>
-
-              <span>
-                Lorem, ipsum dolor sit amet consectetur adipisicing elit. Nemo,
-                tempore.
-              </span>
-            </div>
+          <div className="card">
+            <h2 className="title">Learn</h2>
+            <p>Learn about Rasengan in an interactive course with quizzes!</p>
+            <Link to="https://rasengan.dev/learn" target="_blank">
+              Take the Course
+            </Link>
           </div>
-        </section>
 
-        <div className="powered">
-          <p>
-            Powered by{" "}
-            <a href="https://rasengan.com" target="_blanc">
-              Rasengan
-            </a>
-          </p>
+          <div className="card">
+            <h2 className="title">Examples</h2>
+            <p>Discover and deploy boilerplate example Rasengan projects.</p>
+            <Link to="https://rasengan.dev/examples" target="_blank">
+              View Examples
+            </Link>
+          </div>
+
+          <div className="card">
+            <h2 className="title">Community</h2>
+            <p>Join an active community of Rasengan users on Discord.</p>
+            <Link to="https://rasengan.dev/discord" target="_blank">
+              Join Discord
+            </Link>
+          </div>
         </div>
       </section>
     );
